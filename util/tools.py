abe8eac270fba66219efe822de3a6c7422a7d824
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2022-04-13 10:04:28 +0100

    Update tools.py

--- util/tools.py
+++ util/tools.py
@@ -1,29 +1,49 @@
-import subprocess
 import os
-import json
-import time
+import simplejson
+from time import sleep
 from includes.config import *
+from subprocess import PIPE, Popen
 
 
-def getNodeStats() -> str:
-    mode = ""
-    for x in range(5):
-        try:
-            ourVersion = subprocess.getoutput(f"{envs.HARMONY_FOLDER}/hmy utility metadata")
-            str1 = json.loads(ourVersion)
-            if "result" in str1:
-                if "consensus" in str1["result"]:
-                    mode = str1["result"]
-                    return mode
-                    break
-                else:
-                    time.sleep(3) 
-            else:
-                time.sleep(3)  
-        except:
-            mode = "error"
-            log.error("Node stats - Signing mode could not be found")
-    return mode 
+
+def get_json_for_command(process_args, retries=3, retry_wait=0.1):
+    original_process_args = process_args[:]
+    process = Popen(process_args, stdout=PIPE)
+    (output, err) = process.communicate()
+    try:
+        routput = simplejson.loads(output)
+        return routput['result']
+    except simplejson.JSONDecodeError:
+        sleep(retry_wait)
+        log.error(f"Got an error in get_json_for_command({' '.join(process_args)}), output={output}, err={err}, "
+              f"retrying after {retry_wait}s")
+        if retries > 0:
+            return get_json_for_command(original_process_args, retries=retries - 1, retry_wait=retry_wait * 1.25)
+    return None
+ 
+def getNodeStats():
+    return get_json_for_command([envs.HARMONY_FOLDER+"/hmy", "utility", "metadata"])
+    
+        
+# def getNodeStats() -> dict:
+#     mode = ""
+#     for x in range(5):
+#         try:
+#             ourVersion = subprocess.getoutput(f"{envs.HARMONY_FOLDER}/hmy utility metadata")
+#             str1 = json.loads(ourVersion)
+#             if "result" in str1:
+#                 if "consensus" in str1["result"]:
+#                     mode = str1["result"]
+#                     return mode
+#                     break
+#                 else:
+#                     time.sleep(3) 
+#             else:
+#                 time.sleep(3)  
+#         except:
+#             mode = "error"
+#             log.error("Node stats - Signing mode could not be found")
+#     return mode 
     
     
     
