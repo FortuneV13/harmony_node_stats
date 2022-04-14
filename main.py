1bcbffd1a199ec53070811dab64b6f3a9a51f993
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2022-04-14 21:37:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,43 +18,48 @@ count = 0
 while True:
     # increment count
     count = count + 1
-    # Set default node heights
-    send_shard_0_remote = 0
-    send_shard_0_local = 0
-    send_shard_main_remote = 0
-    send_shard_main_local = 0
+    # Set defaults
+    load = None
+    send_shard_0_remote = None
+    send_shard_0_local = None
+    send_shard_main_remote = None
+    send_shard_main_local = None
    
         
     try:
         # Get Server Load
-        load = os.getloadavg()
-
+        try:
+            load = os.getloadavg()
+        except Exception as e:
+            load = None
+        
         # Get Node utility metadata
         node_stats = getNodeStats()
-
         # Get Shard ID from node
         shard = node_stats['shard-id']
         
-        # Shard 0 - Remote
-        result_shard_0_remote = getSyncRemote(f'https://api.s0.t.hmny.io')
-        send_shard_0_remote =  literal_eval(result_shard_0_remote['shard-chain-header']['number'])
-        
-        # Shard Main - Remote
-        result_shard_main_remote = getSyncRemote(f'https://api.s{shard}.t.hmny.io')
-        send_shard_main_remote =  literal_eval(result_shard_main_remote['shard-chain-header']['number'])
-        
-        # Locals
-        result_local_shard = getSyncLocal()
-        send_shard_0_local =  literal_eval(result_local_shard['beacon-chain-header']['number'])
-        send_shard_main_local =  literal_eval(result_local_shard['shard-chain-header']['number'])
-  
-        # Space left
         try:
+            # Shard 0 - Remote
+            result_shard_0_remote = getSyncRemote(f'https://api.s0.t.hmny.io')
+            send_shard_0_remote =  literal_eval(result_shard_0_remote['shard-chain-header']['number'])
+            # Shard Main - Remote
+            result_shard_main_remote = getSyncRemote(f'https://api.s{shard}.t.hmny.io')
+            send_shard_main_remote =  literal_eval(result_shard_main_remote['shard-chain-header']['number'])
+            # Locals
+            result_local_shard = getSyncLocal()
+            send_shard_0_local =  literal_eval(result_local_shard['beacon-chain-header']['number'])
+            send_shard_main_local =  literal_eval(result_local_shard['shard-chain-header']['number'])
+        except Exception as e:
+            send_shard_0_remote = None
+            send_shard_0_local = None
+            send_shard_main_remote = None
+            send_shard_main_local = None
+            
+        try:
+            # Space left
             space = subprocess.check_output('df -BG --output=avail "$PWD" | tail -n 1', shell=True).decode(sys.stdout.encoding)
         except Exception as e:
-            log.error(e) 
-            log.error(f"Please fix me! - Space left func error")
-            alerts.generic_error(e)
+            space = None
             
         # # Send to vStats
         alerts.send_to_vstats(node_stats, send_shard_0_remote, send_shard_0_local, send_shard_main_remote, send_shard_main_local, load,space,count)
