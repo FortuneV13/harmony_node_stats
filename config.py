d5d0267f86c9cbf9c6412ec53467defd5656c9b1
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2022-08-09 10:52:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -18,27 +18,27 @@ SPACE_CHECK_ENABLED=True
 
 #Shard Data 
 #- Comment/Uncomment as necessary 
-#- Comment out http_port if NOT running a multiple shard server
+#- Change http_port if running a multiple shard server
 SHARD_ARRAY = {
 	 
 	# "shard-0":{
 	# 	"harmony_folder":"/home/serviceharmony/harmony",
-	# 	"node_port":"" #optional - only if running multi shard server
+	# 	"node_port":9501 
 	# },
 	 
 	"shard-1":{
 		"harmony_folder":"/home/serviceharmony/harmony",
-		"http_port":"" #optional - only if running multi shard server
+		"http_port":9501 
 	},
 	
 	# "shard-2":{
 	# 	"harmony_folder":"/home/serviceharmony/harmony",
-	# 	"http_port":"" #optional - only if running multi shard server
+	# 	"http_port":9501 
 	# },
 	
     # "shard-3":{
 	# 	"harmony_folder":"/home/serviceharmony/harmony",
-	# 	"http_port":"" #optional - only if running multi shard server
+	# 	"http_port":9501 
 	# },
 	
 }
