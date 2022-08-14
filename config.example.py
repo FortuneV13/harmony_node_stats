10a2af4e658d5ca899e9d71befde15050bbe10f6
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2022-08-14 13:36:51 +0100

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -20,12 +20,12 @@ SPACE_CHECK_ENABLED=True
 #- Comment/Uncomment as necessary 
 #- Change http_port if running a multiple shard server #9501 is the default harmony.conf port under HTTP
 SHARD_ARRAY = {
-	 
+
 	"first-shard":{
 		"harmony_folder":"/home/serviceharmony/harmony",
 		"node_port":9501 
 	},
-	 
+	
 	# "second-shard":{
 	# 	"harmony_folder":"/home/serviceharmony/harmony",
 	# 	"http_port":9501 
