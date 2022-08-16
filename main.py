b2de33557a71c9c88fe45e1774b6a2d5cbeb668b
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2022-08-16 11:35:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ while True:
                 
                 try:
                     # Shard 0 - Remote
-                    result_shard_0_remote = getSyncRemote(shardValue,f'https://api.s0.t.hmny.io')
-                    send_shard_0_remote =  literal_eval(result_shard_0_remote['shard-chain-header']['number'])
+                    # result_shard_0_remote = getSyncRemote(shardValue,f'https://api.s0.t.hmny.io')
+                    # send_shard_0_remote =  literal_eval(result_shard_0_remote['shard-chain-header']['number'])
                     # Shard Main - Remote
                     # result_shard_main_remote = getSyncRemote(shardValue,f'https://api.s{shard}.t.hmny.io')
                     # send_shard_main_remote =  literal_eval(result_shard_main_remote['shard-chain-header']['number'])
