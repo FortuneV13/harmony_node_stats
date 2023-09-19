306b1239f1af3e53c7b3cf0a444c04cc1f44f42e
DavidWhicker <51021366+FortuneV13@users.noreply.github.com>
2023-09-19 12:52:06 +0100

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -7,21 +7,12 @@ VSTATS_TOKEN=""
 #- Comment/Uncomment as necessary 
 #- Change http_port if running a multiple shard server #9500 is the default harmony.conf port under HTTP
 SHARD_ARRAY = {
-	"first-shard":{
+	"S0":{
 		"harmony_folder":"/home/serviceharmony/harmony",
 		"http_port":9500,
 	},
-	# "second-shard":{
+	# "S1":{
 	# 	"harmony_folder":"/home/serviceharmony/harmony",
 	# 	"http_port":9501,
 	# },
-	# "third-shard":{
-	# 	"harmony_folder":"/home/serviceharmony/harmony",
-	# 	"http_port":9501,
-	# },
-	# "forth-shard":{
-	# 	"harmony_folder":"/home/serviceharmony/harmony",
-	# 	"http_port":9501,
-	# },
-	
 }
