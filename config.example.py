667e3050632e05e30c713a8b23c0543d832d9f3f
Fortune <51021366+FortuneV13@users.noreply.github.com>
2024-02-11 14:12:22 +0000

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -7,8 +7,8 @@ NODE_ARRAY = [
     {
         "unique_name": "S0", # Keep it short | Used in node_stats summaries to identify node | Must be unique
         "shard":0, # Fall back to send to vstats if metadata fails to load . i.e service not running
-        "service_name":"harmony",
-        "harmony_folder":"/home/serviceharmony/harmony",
-        "http_port":9500,
+        "service_name":"harmony", # Default is harmony
+        "harmony_folder":"/home/serviceharmony/harmony", # Path to your harmony binary dir
+        "http_port":9500, # Default is 9500
     }
 ]
